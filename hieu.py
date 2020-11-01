19a5ea9a23a60b7659d202f9417de04e33e85b85
khoipham1582 <khoipham1582@gmail.com>
2020-11-01 21:13:29 +0700

    fix linh tinh

--- hieu.py
+++ hieu.py
@@ -1,6 +1,10 @@
 from datetime import date,datetime
+import time
 #print(datetime.now().strftime("%H-%m-%S"))
 now=datetime.now()
 #now=now.time()
 current_time=str(now.hour)+" giờ "+str(now.minute)+" phút "+str(now.second)+" giây."
 print(current_time)
+file=str(now.time()).replace(":","")
+file=file.replace(".","")
+print(file)
--- robotvn.py
+++ robotvn.py
@@ -22,7 +22,7 @@ while(True):
     if(you==""):
         robot_brain="Bạn không muốn nói chuyện sao"
     elif "chào" in you:
-        robot_brain="Chào bạn tôi là robot việt nam"
+        robot_brain="Chào bạn tôi là robot funny tôi có thể nghe hiểu và nói tiếng việt"
     elif 'ngày' in you:
         today=date.today()
         robot_brain="Ngày "+str(today.day)+" tháng "+str(today.month)+" năm "+str(today.year)
@@ -30,29 +30,45 @@ while(True):
         now=datetime.now()
         current_time=str(now.hour)+" giờ "+str(now.minute)+" phút "+str(now.second)+" giây."
         robot_brain=current_time
-    elif "thủ tướng" in you:
+    elif "Thủ tướng" in you:
         robot_brain="Thủ tướng nước việt nam hiện nay là Nguyễn Xuân Phúc"
-    elif "tạm biệt" in you:
-        robot_brain="Tạm biệt bạn"
-        filemp3="mp3/"+datetime.now().strftime("%H-%m-%S")+".mp3"
+    elif "Chủ tịch nước" in you:
+        robot_brain="chủ tịch nước việt nam hiện nay là Nguyễn Phú Trọng"
+    elif "Chủ tịch quốc hội" in you:
+        robot_brain="chủ tịch quốc hội nước việt nam hiện nay là Nguyễn Thị Kim Ngân"
+    elif "Ai tạo ra bạn" in you:
+        robot_brain="Ông chủ khôi phạm"
+    elif "Tên bạn " in you:
+        robot_brain="tên tôi là robot funny"
+    elif "tạm biệt" in you or "thoát" in you:
+        robot_brain="Tạm biệt bạn, chúc ngủ ngon"
+        tts=gTTS(robot_brain,lang='vi')
+        filemp3="mp3/"+datetime.now().strftime("%H-%m-%S")+str(random.randint(0,1000))+".mp3"
         tts.save(filemp3)
         playsound(filemp3)
         break
     elif "Thủ đô" in you:
         robot_brain="Thủ đô việt nam là Hà Nội"
     elif "Thời tiết" in you:
-        robot_brain="Thời tiết hôm nay có mưa, giông bão cấp 12, 13 bạn nên cẩn thận khi ra ngoài"
+        robot_brain="Thời tiết hôm nay có mưa, giông bão cấp 12 bạn nên cẩn thận khi ra ngoài"
     elif "Khang" in you:
         robot_brain="Khang là thằng mập ú thích ẳng ẳng, tham ăn lười làm"
-    elif "Bơ" in you:
-        robot_brain="Bơ là bé múp míp, hay bơ bơ, hậu đậu"
+    elif "ú" in you:
+        robot_brain="Là thằng Khang mập ú thích ẳng ẳng, tham ăn lười làm"
+    elif "bơ" in you:
+        robot_brain="Bé Lam Bơ là bé múp míp, hay bơ bơ, hậu đậu"
+    elif "bé bơ" in you:
+        robot_brain="Bé Lam Bơ là bé múp míp, hay bơ bơ, hậu đậu"
+    elif "bé bơ" in you:
+        robot_brain="Bé Lam Bơ là bé múp míp, hay bơ bơ, hậu đậu"
     else: robot_brain="Bạn có thể nói lại không, tôi nghe không rõ"
     print("Robot :"+robot_brain)
-    
     try:
         tts=gTTS(robot_brain,lang='vi')
         #filemp3=random.randint(0,1000)
-        filemp3="mp3/"+datetime.now().strftime("%H-%m-%S")+".mp3"
+        #file1=str(now.time()).replace(":","")
+        #file1=file1.replace(".","")
+        filemp3="mp3/"+datetime.now().strftime("%H-%m-%S")+str(random.randint(0,1000))+".mp3"
         tts.save(filemp3)
         playsound(filemp3)
     except:
